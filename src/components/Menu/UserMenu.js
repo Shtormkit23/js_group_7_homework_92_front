d771b4f7df00bdb6cc2ba1b835b34060cf2d92db
maksimus <gorlovmaks23@gmail.com>
2020-12-04 17:27:51 +0600

    Добавила временное меню для залогиненных и не залог. пользователей

---
+++ src/components/Menu/UserMenu.js
@@ -0,0 +1,59 @@
+import React from "react";
+import {Button} from "@material-ui/core";
+import {useDispatch} from "react-redux";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import {makeStyles} from "@material-ui/core/styles";
+import {logoutUser} from "../../store/actions/usersActions";
+
+
+
+const useStyles = makeStyles(theme => ({
+    dropDownBtn: {
+        color: theme.palette.common.white
+    }
+}));
+
+const UserMenu = ({user}) => {
+    const dispatch = useDispatch();
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const logout = () => {
+        dispatch(logoutUser());
+    };
+
+    return (
+        <>
+            <Button
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+                className={classes.dropDownBtn}
+            >
+                Hello, {user.displayName ? user.displayName : user.username}
+            </Button>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <MenuItem>Profile</MenuItem>
+                <MenuItem>My account</MenuItem>
+                <MenuItem onClick={logout}>Logout</MenuItem>
+            </Menu>
+        </>
+    );
+};
+
+export default UserMenu;
