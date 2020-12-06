b3346d5fcb77730942fe4daa9110e9e203db7d94
maksimus <gorlovmaks23@gmail.com>
2020-12-07 02:04:29 +0600

    Добавила компонент для вывода активных пользователей

---
+++ src/components/activeUser/activeUser.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ActiveUser = props => (
+        <div className="activeUser">
+                <p className="user">{props.user}</p>
+        </div>
+);
+
+export default ActiveUser;
