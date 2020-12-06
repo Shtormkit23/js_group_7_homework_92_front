b1b0d5019a6890419d7562a6db87205d09834787
maksimus <gorlovmaks23@gmail.com>
2020-12-07 02:13:50 +0600

    Добавила роуты для перехода по главныи страницам приложения

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Container from "@material-ui/core/Container";
 import AppToolbar from "./components/UI/AppToolbar/AppToolbar";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import {useSelector} from "react-redux";
+import Login from "./containers/Login/Login";
+import Register from "./containers/Register/Register";
+import "./App.css";
+import Chat from "./containers/Chat/Chat";
+
 
 const App = () => {
     const user = useSelector(state => state.users.user);
@@ -14,7 +19,10 @@ const App = () => {
             <main>
                 <Container>
                     <Switch>
-                        <Route path="/" exact component={AppToolbar} />
+                        <Route path="/" exact component={Login} />
+                        <Route path="/login" exact component={Login} />
+                        <Route path="/chat" exact component={Chat} />
+                        <Route path="/register" exact component={Register} />
                     </Switch>
                 </Container>
             </main>
