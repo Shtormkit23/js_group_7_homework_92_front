fabf9391110419e6ee9c57ce3123b197a9c13aa8
maksimus <gorlovmaks23@gmail.com>
2020-12-07 17:31:20 +0600

    Привязала значение к стейту

--- src/components/EnteringMessage/EnteringMessage.js
+++ src/components/EnteringMessage/EnteringMessage.js
@@ -1,16 +1,25 @@
 import React from "react";
 import './EnteringMessage.css';
 
-const EnteringMessage = props => (
+
+const EnteringMessage = props => {
+    return (
     <form onSubmit={props.addMessage} className="form">
         <div className="form-group">
             <div className="column-2">
-                <textarea name="text" className="form-control message-form message-input" placeholder="Текст сообщения"
-                          id="message-form" onChange={props.changeMessage}/>
+                <textarea
+                    required={true}
+                    name="text"
+                    className="form-control message-form message-input"
+                    placeholder="Текст сообщения"
+                    id="message-form" onChange={props.changeMessage}
+                value={props.value}
+                />
             </div>
         </div>
         <button type="submit" className="btn-send" id="button">Send</button>
     </form>
-);
+    );
+};
 
 export default EnteringMessage;
